81a53179b2643d6309b1e864cc586b857a16aa58
baidyanath-p <baidyanath.p@homelane.com>
2020-04-19 22:00:34 +0530

    Few more concepts added

--- cluster-demo.js
+++ cluster-demo.js
@@ -14,12 +14,30 @@ if(cluster.isMaster){
     for(let i = 0; i < noOfCpus; i++){
         cluster.fork();
     }
+
+    // on exit process
+    cluster.on('exit', worker => {
+        console.log(`worker process ${process.pid} had died`);
+
+        // show no of remaining worker
+        console.log(`only ${Object.keys(cluster.workers).length} remaining`);
+
+        //start new worker
+        console.log(`starting new worker`);
+        cluster.fork();
+    })
 }
 else {
+    console.log(`started a worker at ${process.pid}`);
     http.createServer((req, res) => {
-        const message = `Worker ${process.pid}`;
-        console.log(message);
-        res.end(message);
+        res.end(`Process: ${process.pid}`);
+
+        if(req.url === '/kill'){
+            process.exit();
+        } 
+        else if (req.url === '/'){
+            console.log(`servering from ${process.pid}`);
+        }
     }).listen(3000);
 }
 
