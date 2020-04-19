24b23cceb6d49f1ec77d5522f181a8f971704c60
baidyanath-p <baidyanath.p@homelane.com>
2020-04-19 14:53:52 +0530

    New endpoint added which gives random advices

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require('http');
 const fs = require('fs')
 
+const port = parseInt(process.argv[2] || '3000');
+
+const options = [
+    'I can not help you!',
+    'Sure, I will help you',
+    'Go, this way',
+    'Go, that way'
+];
+
 const server = http.createServer((req, res) => {
+
+        // Home page as Hello People
         if(req.url === '/'){
             fs.readFile('resource/MyPage.html', (err, pageResp) => {
                 if(err){
@@ -15,8 +26,21 @@ const server = http.createServer((req, res) => {
                 res.end();
             })            
         }
+        // New endpoint `help` which provides random advice
+        else if (req.url == '/help'){
+            const randomIndex = Math.floor(Math.random() * options.length);
+            const resModel = JSON.stringify({
+                port,
+                processID: process.pid,
+                advice: options[randomIndex]
+            })
+
+            res.writeHead(200, {'Content-Type': 'text/json'});
+            res.write(resModel)
+            res.end();
+        }
 })
 
 
-server.listen(3000);
-console.log('Listening on port 3000..')
+server.listen(port);
+console.log(`Listening on port ${port}..`)
